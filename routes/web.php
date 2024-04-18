9f9cca8dbee16b7e0bcff514604305046b3a530b
James Arnold <jamesnogra@gmail.com>
2024-04-19 00:33:25 +0800

    Change homepage to register.

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\SongController;
 
 Route::get('/', function () {
-    return view('music');
+    return view('register');
 });
 
 /** Users */
